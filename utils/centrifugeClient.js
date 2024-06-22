463dc21822ee65590a6a861a50bfef12926a9a2d
vrinch <odukwevince76@gmail.com>
2024-06-23 00:12:50 +0100

    minor update on centrifuge

--- utils/centrifugeClient.js
+++ utils/centrifugeClient.js
@@ -3,45 +3,78 @@
 import { Centrifuge } from 'centrifuge';
 import axios from 'axios';
 
-const centrifuge = new Centrifuge('wss://api.testnet.rabbitx.io/ws');
+// const centrifuge = new Centrifuge('wss://api.testnet.rabbitx.io/ws');
 
-const authenticateCentrifuge = async () => {
-  try {
-    const response = await axios.post('https://api.testnet.rabbitx.io/auth', {
-      token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwiZXhwIjo1MjYyNjUyMDEwfQ.x_245iYDEvTTbraw1gt4jmFRFfgMJb-GJ-hsU9HuDik',
-    });
+// const authenticateCentrifuge = async () => {
+//   try {
+//     const response = await axios.post('https://api.testnet.rabbitx.io/auth', {
+//       token:
+//         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwiZXhwIjo1MjYyNjUyMDEwfQ.x_245iYDEvTTbraw1gt4jmFRFfgMJb-GJ-hsU9HuDik',
+//     });
 
-    return response.data;
-  } catch (error) {
-    console.error('Error authenticating with Centrifuge:', error);
-    throw error;
-  }
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error authenticating with Centrifuge:', error);
+//     throw error;
+//   }
+// };
+
+// export const connectToCentrifuge = async () => {
+//   try {
+//     const authData = await authenticateCentrifuge();
+
+//     centrifuge.setToken(authData.token);
+
+//     return new Promise((resolve, reject) => {
+//       centrifuge.on('connect', () => {
+//         console.log('Connected to Centrifuge');
+//         resolve();
+//       });
+
+//       centrifuge.on('disconnect', () => {
+//         console.log('Disconnected from Centrifuge');
+//         reject();
+//       });
+
+//       centrifuge.connect();
+//     });
+//   } catch (error) {
+//     console.error('Error connecting to Centrifuge:', error);
+//     throw error;
+//   }
+// };
+
+// export default centrifuge;
+
+const myWs = function (options) {
+  return class wsClass extends WebSocket {
+    constructor(...args) {
+      if (args.length === 1) {
+        super(...[...args, 'centrifuge-json', ...[options]]);
+      } else {
+        super(...[...args, ...[options]]);
+      }
+    }
+  };
 };
 
-export const connectToCentrifuge = async () => {
-  try {
-    const authData = await authenticateCentrifuge();
+let centrifuge = new Centrifuge(
+  'ws://192.168.1.195:8000/connection/websocket',
+  {
+    websocket: myWs({ headers: { Authorization: '<token or key>' } }),
+  },
+);
 
-    centrifuge.setToken(authData.token);
+centrifuge.on('connected', function (ctx) {
+  console.log('connected', ctx);
+});
 
-    return new Promise((resolve, reject) => {
-      centrifuge.on('connect', () => {
-        console.log('Connected to Centrifuge');
-        resolve();
-      });
+centrifuge.on('connecting', function (ctx) {
+  console.log('connecting', ctx);
+});
 
-      centrifuge.on('disconnect', () => {
-        console.log('Disconnected from Centrifuge');
-        reject();
-      });
+centrifuge.on('disconnected', function (ctx) {
+  console.log('disconnected', ctx);
+});
 
-      centrifuge.connect();
-    });
-  } catch (error) {
-    console.error('Error connecting to Centrifuge:', error);
-    throw error;
-  }
-};
-
-export default centrifuge;
+centrifuge.connect();
